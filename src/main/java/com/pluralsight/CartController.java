d86b969f2369e0caf0aeaf0eae8b7d002fa8ecd1
Vytautas Ambrasas <chatoman@gmail.com>
2019-05-21 14:21:52 +0300

    Completed module

--- src/main/java/com/pluralsight/CartController.java
+++ src/main/java/com/pluralsight/CartController.java
@@ -44,7 +44,10 @@ public class CartController extends HttpServlet {
 			switch(action) {
 				case "/addcart":
 					 addToCart(request, response);
-           break;
+				break;
+				case "/delete":
+					deleteFromCart(request, response);
+           		break;
         default:
            break;
 			}
@@ -56,7 +59,16 @@ public class CartController extends HttpServlet {
 		response.sendRedirect("../ShoppingCart.jsp");
 	}
 
-  protected void addToCart(HttpServletRequest request, HttpServletResponse response)
+	private void deleteFromCart(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException{
+
+		HttpSession session = request.getSession();
+		int index = Integer.parseInt(request.getParameter("index"));
+		ShoppingCart cart = (ShoppingCart)session.getAttribute("cart");
+		cart.deleteCartItem(index);
+	}
+
+	protected void addToCart(HttpServletRequest request, HttpServletResponse response)
 		throws ServletException, IOException {
    HttpSession session = request.getSession();
    String idStr = request.getParameter("id");
@@ -82,6 +94,7 @@ public class CartController extends HttpServlet {
 	 // Add this item and quantity to the ShoppingCart
    shoppingCart.addCartItem(existingBook, quantity);
   }
+
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
